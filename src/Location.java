bddaf87ec27adb23dbe05b8c6e62d4a89f526498
Benjamin CHEN <BenjaminCHEN2016@outlook.com>
2017-11-30 15:32:03 +0800

    add: json lib database resource config

---
+++ src/Location.java
@@ -0,0 +1,74 @@
+public class Location {
+    private String title;
+    private int id;
+    private double latitude;
+    private double longitude;
+    private String iconPath;
+    private int width;
+    private int height;
+
+    public Location(int id, String name, double latitude, double longtitude) {
+        this.title = name;
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longtitude;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIconPath() {
+        return iconPath;
+    }
+
+    public void setIconPath(String iconPath) {
+        this.iconPath = iconPath;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+
+}
